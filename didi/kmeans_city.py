21a2f3d9f9517672c036df986bf6ae49ceec1731
wangchunliang <wangchunliang@didichuxing.com>
2017-05-22 23:12:01 +0800

    modify kmeans_city

--- didi/kmeans_city.py
+++ didi/kmeans_city.py
@@ -6,17 +6,12 @@ from sklearn.cluster import KMeans ,k_means
 from scipy import sparse
 from sklearn.externals import joblib
 
-def city_cluster():
+def city_cluster(city_data):
     
-    city_data=np.loadtxt('/Users/didi/Documents/city_cluster/city_cluster')
+    #city_data=np.loadtxt('/Users/didi/Documents/city_cluster/city_cluster')
     x_p=city_data[:,:4]
-    #y_p=city_data[:,2]
-    #X=(sparse.csc_matrix((x_p)).astype(float))[:,:].todense()
-    #print X
-    #print x_p
-    #print x_p
     
-    clf=KMeans(n_clusters=3)
+    clf=KMeans(n_clusters=4)
     xp_pred=clf.fit(x_p)
     
     #print clf.labels_
@@ -26,7 +21,7 @@ def city_cluster():
     #进行预测
     print clf.predict(x_p)
     #保存模型
-    joblib.dump(clf , '/Users/didi/Documents/city_cluster/km0_3.pkl')
+    joblib.dump(clf , '/Users/didi/Documents/city_cluster/km0522_03.pkl')
     #载入保存的模型
     #clf = joblib.load('/Users/didi/Documents/city_cluster/')
     
@@ -42,6 +37,27 @@ def city_cluster():
         print  clf.labels_[i-1]  
         i = i + 1  
     print label_dict
-
+    
+def city_pridict(city_data):
+    x_p=city_data[:,:4]
+    clf = joblib.load('/Users/didi/Documents/city_cluster/km1.pkl')
+    clf.predict(x_p)
+    
+    i = 1  
+    label_dict={}
+    j=1
+    while j <= len(clf.labels_):  
+        label_dict[clf.labels_[j-1]]=0
+        j = j+1
+        
+    while i <= len(clf.labels_):  
+        label_dict[clf.labels_[i-1]]=label_dict[clf.labels_[i-1]]+1
+        print  clf.labels_[i-1]  
+        i = i + 1  
+    print label_dict
+    
 if __name__=="__main__":
-    city_cluster()
+    city_data=np.loadtxt('/Users/didi/Documents/city_cluster/city_cluster')
+    city_cluster(city_data)
+    #city_pridict(city_data)
+    
